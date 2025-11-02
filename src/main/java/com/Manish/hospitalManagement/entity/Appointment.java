9aedf9f1fc5f7da9da55c05f8c5c95ca6397fd34
Manish <manishlodhi0224@gmail.com>
2025-11-02 11:31:27 +0530

    appointment entity

---
+++ src/main/java/com/Manish/hospitalManagement/entity/Appointment.java
@@ -0,0 +1,30 @@
+package com.Manish.hospitalManagement.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Appointment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDateTime appointmentTime;
+
+    @Column(length = 50)
+    private String reason;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Patient patient;
+}
