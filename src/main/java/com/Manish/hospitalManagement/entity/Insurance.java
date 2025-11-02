16147dcf9c8867214e7aa955f72dc5becb66b1a5
Manish <manishlodhi0224@gmail.com>
2025-11-02 11:32:36 +0530

    insurance entity

---
+++ src/main/java/com/Manish/hospitalManagement/entity/Insurance.java
@@ -0,0 +1,40 @@
+package com.Manish.hospitalManagement.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Insurance {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String policyNumber;
+
+    @Column(nullable = false, length = 100)
+    private String provider;
+
+    @Column(nullable = false)
+    private LocalDate validUntil;
+
+    @CreationTimestamp    // automatic fill the value of createdAt fields whenever it is made
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @OneToOne(mappedBy = "insurance")   // inverse side...bcoz foreign key pe control nhi hai
+    private Patient patient;
+}
